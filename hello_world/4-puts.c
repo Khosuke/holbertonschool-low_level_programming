eff63b45de9283a815c124cabc19bf0574995878
Khosuke <9814@holbertonstudents.com>
2024-10-21 08:30:15 +0000

    C program that print some text

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+char cisfun="\"Programming is like building a multilingual puzzle"
+int main(void){
+	puts(%c\n, cisfun);
+	return (0);
+}
