1bc4708d885c0b44bb6bdf3ec7ef9d9e56951256
Khosuke <9814@holbertonstudents.com>
2024-10-23 07:44:10 +0000

    function to check if character is lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - Check if character is lowercase
+ *
+ *
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
