4d29f376fd039218d7e3d44b248dd4a6e6241c01
Khosuke <9814@holbertonstudents.com>
2024-10-23 09:44:33 +0000

    Return absolute value of integer

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,18 +2,18 @@
 
 /**
  * _abs - return absolute value of integer
- * 
+ * @n number to check
  *
- *
+ * Return: n if positive, n*-1 if negative
  */
 int _abs(int n)
 {
 	if (n < 0)
 	{
-		return(n*-1);
+		return (n * -1);
 	}
 	else
 	{
-		return(n);
+		return (n);
 	}
 }
