e9e9c536ea72fe43e64926a7fc5a8f3f746b2122
Khosuke <9814@holbertonstudents.com>
2024-10-24 13:04:40 +0000

    function to sum two numbers

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
+ * add - sum of two numbers 
+ * @num1: first number
+ * @num2: second number
  *
- *
- *
+ * Return: return the sum of num1 and num2
  */
 int add(int num1, int num2)
 {
