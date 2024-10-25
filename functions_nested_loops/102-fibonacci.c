20989e334e53f404f316b659e9ed0a220f612d75
Khosuke <9814@holbertonstudents.com>
2024-10-25 13:44:39 +0000

    Fibonacci

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
 /**
- * main -
+ * main - print fibonacci
  *
- *
+ * Return: always 0
  */
 int main(void)
 {
 	unsigned long num1 = 0, num2 = 1, new, i;
 
-	for (i = 0; i <= 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		new = num1 + num2;
 		num1 = num2;
 		num2 = new;
 		printf("%ld, ", new);
+		if (i == 49)
+		{
+			printf("%ld", new);
+		}
 	}
 	printf("\n");
 	return (0);
