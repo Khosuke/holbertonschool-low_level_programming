4ae73844e0edad0235d152dfe46da68d598f5550
Khosuke <9814@holbertonstudents.com>
2024-10-28 08:58:41 +0000

    Function to print from 0 to 14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void more_numbers(void)
+{
+	int i, num;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+
+	}
+}
