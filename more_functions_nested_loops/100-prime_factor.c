29995f87c1a49a7edcd2a5a538cf186a93dffcaf
Khosuke <9814@holbertonstudents.com>
2024-10-28 14:26:08 +0000

    Search larget prime factor of a number

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - search largest prime factor of number
+ *
+ */
+int main(void)
+{
+	unsigned long i, n;
+
+	n = 612852475143;
+        i = 2;
+        while (n != 1)
+        {
+		if (n % i == 0)
+		{
+			n = n / i;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%ld\n", i);
+	return (0);
+}
