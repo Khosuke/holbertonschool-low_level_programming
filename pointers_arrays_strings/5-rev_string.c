6979572d37fc5d68141da40429e740d79e8acdc4
Khosuke <9814@holbertonstudents.com>
2024-10-30 09:11:27 +0000

    Reverse a string in an array

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	/* We go through half the lenght of the array */
 	for (i = 0, j = len - 1; i < j; i++, j--)
 	{
-		tmp = s[i]; /* Store the first half of the array*/ 
+		tmp = s[i]; /* Store the first half of the array*/
 		s[i] = s[j]; /* Attributes the last half to first half reversed*/
 		s[j] = tmp; /* Attributes stored value of first half to last half reversed*/
 	}
