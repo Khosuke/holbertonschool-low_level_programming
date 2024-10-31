5bb1ba0357f48c9e030c59a3fa62d176270bcf94
Khosuke <9814@holbertonstudents.com>
2024-10-31 21:58:16 +0000

    Concatenate two strings

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,24 +4,24 @@
  **_strcat - function to concatenate two strings
  * @dest: the string that gets another string at the end of it
  * @src: the string that gets appended
- *  
+ *
  * Return: pointer to result string dest
  */
 char *_strcat(char *dest, char *src)
 {
-        int i = 0, j = 0;
+	int i = 0, j = 0;
 
-        while (dest[i] != '\0')
-        {
-                i++;
-        }
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-        while (src[j] != '\0')
-        {
-                dest[i + j] = src[j];
-                j++;
-        }
-        dest[i + j] = '\0';
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
 
-        return dest;
+	return dest;
 }
