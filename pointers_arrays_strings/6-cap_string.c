abe75eeb08e8119da17eea08fc30feca702206c5
Khosuke <9814@holbertonstudents.com>
2024-11-03 21:52:03 +0000

    Function to capitalize each words in string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,6 +10,10 @@ char *cap_string(char *str)
 {
 	int i;
 
+	if(str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] = str[0] - 32;
+	}
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] == 32 || str[i] == 10 || str[i] == 9)
@@ -22,4 +26,3 @@ char *cap_string(char *str)
 	}
 	return (str);
 }
-
