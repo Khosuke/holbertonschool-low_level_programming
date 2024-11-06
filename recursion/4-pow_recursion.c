2da96558ebc0a5220838ec68e0e4c419280501ef
Khosuke <9814@holbertonstudents.com>
2024-11-06 16:03:34 +0100

    function that returns the value of x raised to the power of y

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * _pow_recursion -
- * 
- * 
+ * _pow_recursion - calc the power of a number
+ * @x: number to be raised to the power of y
+ * @y: the wanted power
+ *
+ * Return: Value of 'x' by power of 'y' or '-1' if 'y' < 0
  */
 int _pow_recursion(int x, int y)
 {
@@ -17,7 +19,7 @@ int _pow_recursion(int x, int y)
 	}
 	if (x == 0)
 	{
-			return (0);
+		return (0);
 	}
 	return (x * (_pow_recursion(x, y - 1)));
 }
