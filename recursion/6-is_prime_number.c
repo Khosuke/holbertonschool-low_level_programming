a885936a721573fab7121216890aa7e9bd79e965
Khosuke <9814@holbertonstudents.com>
2024-11-07 16:46:01 +0100

    Function to check if a number is prime

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_prime - checks if a number is prime
+ * @n: number we're searching 
+ * @x: number to check 
+ *
+ * Return: n and it's square root
+ */
+int is_prime(int n, int x)
+{
+	if (n % x == 0)
+	{
+		return (0);
+	}
+	if (x * x > n)
+	{
+		return (1);
+	}
+	return (is_prime(n, x + 1));
+}
+/**
+ * is_prime_number - check if a number is prime
+ * @n: number to check
+ *
+ * Return: 1 if number 'n' is prime else 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (is_prime(n, 2));
+}
+
