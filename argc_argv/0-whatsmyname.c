8f6e7720263d5a688c7ac0219ea5fcd89e39d9bf
Khosuke <9814@holbertonstudents.com>
2024-11-08 13:13:14 +0100

    Program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: number of argument
+ * @argv: no argument to pass
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
