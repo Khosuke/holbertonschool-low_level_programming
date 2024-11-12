7a933e1da9dfdb3d01a2cbf9f7d919e0d5dd0a09
Khosuke <9814@holbertonstudents.com>
2024-11-12 10:14:05 +0100

    Rewrite strdup which duplicate a string to a new allocated space in memory

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Function that allocate a new space in memory
+ * which contains a copy of the string given as a parameter
+ * @str: The string we want to duplicate
+ *
+ * Return: A pointer to the duplicated string
+ * NULL if insufficient memory available or str = NULL
+ */
+char *_strdup(char *str)
+{
+	char *sdup;
+	unsigned int size = 0, i = 0;
+
+	sdup = malloc((size + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	while (i <= size)
+	{
+		sdup[i] = str[i];
+		i++;
+	}
+	return (sdup);
+}
--- malloc_free/README.md
+++ malloc_free/README.md
@@ -1,3 +1,7 @@
 Project 0: C - Malloc, Free
 
-Task 0 - Write a Function that creates an array of chars, and initializes it with a specific char
+Task 0 - Write a Function that creates an array of chars, and initializes it with a specific char.
+File: 0-create_array.c
+
+Task 1 - Write a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+File: 1-strdup.c
