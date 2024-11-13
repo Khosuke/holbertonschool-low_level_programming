f926470c19bf364d102df3da39dd326ada156a14
Khosuke <9814@holbertonstudents.com>
2024-11-13 10:02:15 +0100

    function that allocates memory for an array, using malloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocate memory for an array
+ * @nmemb: number of elements in array
+ * @size: bytes size of element 'nmemb'
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned char* ptr = NULL;
+	unsigned int i = 0;
+	
+	ptr = malloc(nmemb * size);
+	while (i < size)
+	{
+		ptr[i] = '\0';
+		i++;
+	}
+	if (ptr == NULL)
+    	return (NULL);
+	return (ptr);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -5,3 +5,6 @@ File: 0-malloc_checked.c
 
 Task 1 - Write a function that concatenates two strings.
 File: 1-string_nconcat.c
+
+Task 2 - Write a function that allocates memory for an array, using malloc.
+File: 2-calloc.c
