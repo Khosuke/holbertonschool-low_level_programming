ce0f6171eca28fdcbe7075d033aad5076baac34a
Khosuke <9814@holbertonstudents.com>
2024-11-15 12:16:06 +0100

    function that frees dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free the memory after using new_dog
+ * @d: the dog structure to free
+ * 
+ * Return: Void function, no return
+ */
+void free_dog(dog_t *d)
+{
+	if (d->name != NULL)
+		free(d->name);
+	if (d->owner != NULL)
+		free(d->owner);
+	if (d != NULL)
+		free(d);
+}
