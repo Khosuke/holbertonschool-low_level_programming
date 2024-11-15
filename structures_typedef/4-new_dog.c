2318e184ba826d9b546e82bfa484dd1068b9c9d3
Khosuke <9814@holbertonstudents.com>
2024-11-15 11:47:35 +0100

    function to copy a struct dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -27,27 +27,39 @@ int _strlen(char *s)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog = malloc(sizeof(dog_t));
 	int len_name, len_owner, i = 0, j = 0;
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
 
 	len_name = _strlen(name);
 	len_owner = _strlen(owner);
-	new_dog->name = malloc(sizeof(char) * len_name + 1);
+	new_dog->name = malloc(len_name + 1);
 	if (new_dog->name == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-	new_dog->owner = malloc(sizeof(char) * len_owner +1);
+	}
+	new_dog->owner = malloc(len_owner + 1);
 	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
+	}
 	while (i < len_name)
 	{
 		new_dog->name[i] = name[i];
 		i++;
 	}
+	new_dog->name[i] = '\0';
 	while (j < len_owner)
 	{
 		new_dog->owner[j] = owner[j];
 		j++;
 	}
+	new_dog->owner[j] = '\0';
 	new_dog->age = age;
 	return (new_dog);
 }
