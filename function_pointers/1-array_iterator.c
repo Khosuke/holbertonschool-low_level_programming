8c711cd1ff41b17bf8b23c143865dbc41c5c5fe5
Khosuke <9814@holbertonstudents.com>
2024-11-18 10:04:05 +0100

    Changes to fix betty coding style and doc

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
- * array_iterator - 
- * @array: 
- * @size: 
- * @action: 
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: array of integers
+ * @size: size of the array
+ * @action: function to execute
  *
  * Return: No return, void function
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	long unsigned int i = 0;
+	unsigned long int i = 0;
+
 	while (i < size)
 	{
 		(*action)(array[i]);
 		i++;
 	}
-	return;
 }
