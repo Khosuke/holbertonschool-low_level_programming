9c6a5518aae8332cd5a73b997134a825b396bff6
Khosuke <9814@holbertonstudents.com>
2024-12-02 11:02:08 +0100

    edited and fix 0 and 1 files betty coding and document style

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,7 +9,8 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	while(h != NULL)
+
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
-
+/**
+ * list_len - count the number of elements in a linked list_t list
+ * @h: the list
+ *
+ * Return: The number of elements in the list h
+ */
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
-	while(h != NULL)
+
+	while (h != NULL)
 	{
 		h = h->next;
 		count++;
