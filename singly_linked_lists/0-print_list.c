a1ef522ff2c9e964cdba887e89d1b5cc1dc5e2f1
Khosuke <9814@holbertonstudents.com>
2024-12-02 10:44:27 +0100

    Added first task file and header file

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - Print all element of a list_t list.
+ * @h: the list_t list to print
+ *
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count;
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
+		count++;
+	}
+	return (count);
+}
