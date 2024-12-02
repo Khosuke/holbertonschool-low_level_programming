bf540f0a7e70ca19e1cfff397ae46da5dbdc2664
Khosuke <9814@holbertonstudents.com>
2024-12-02 11:26:55 +0100

    Fixed betty coding style of file 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_node - function that add a new node 
+ * add_node - function that add a new node
  * at the beginning of a list_t list
  * @head: the list we want to add element
  * @str: the string to add to the list
@@ -11,7 +11,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
-	if(new_node == NULL)
+
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
