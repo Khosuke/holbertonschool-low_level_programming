62c8382f22bd4d3938865c3135e99604e602fdb2
Khosuke <9814@holbertonstudents.com>
2024-12-03 09:30:23 +0100

    Function to add a node at the end of the linked list

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * _strlen - count the number of element in the string
+ * @str: string to count
+ * Return: the length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - Add a new node at end of linked list
+ * @head: the linked list
+ * @str: the string to add in the new node
+ * 
+ * Return: The address of the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *current = *head;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	new_node->len = _strlen(str);
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	return (new_node);
+}
