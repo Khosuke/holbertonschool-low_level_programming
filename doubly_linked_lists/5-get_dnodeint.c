571c838d23dc07240d4cd68dd0ea851ab926dfb9
Khosuke <9814@holbertonstudents.com>
2024-12-03 15:35:04 +0100

    Added if condition to make sure the node exist

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,8 +11,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while (i <= index - 1)
+	while (i <= index - 1 && head != NULL)
 	{
+		if (head->next == NULL && i != index - 1)
+		{
+			return (NULL);
+		}
 		head = head->next;
 		i++;
 	}
