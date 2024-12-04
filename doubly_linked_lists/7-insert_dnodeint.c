36bc0268500e2e0c77eb486b8011e1cbedee2b75
Khosuke <9814@holbertonstudents.com>
2024-12-04 12:07:44 +0100

    moved 'new_node->prev = temp;' before 'if (temp->next != NULL)'

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->prev = NULL;
-		new_node->next = temp;
+		new_node->next = *h;
 		if (h != NULL)
 			(*h)->prev = new_node;
 		*h = new_node;
@@ -42,11 +42,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		free(new_node);
 		return (NULL);
 	}
+	new_node->prev = temp;
 	if (temp->next != NULL)
 	{
 		temp->next->prev = new_node;
 	}
-	new_node->prev = temp;
 	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
