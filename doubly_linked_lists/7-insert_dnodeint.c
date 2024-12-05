804bc81f3b15d29aaf5d185cf5f0aa92a835a5a5
Khosuke <9814@holbertonstudents.com>
2024-12-05 15:14:44 +0100

    initialiazed new node next and prev to null at beginning of programm

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,11 +18,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
 		new_node->next = *h;
-		if (h != NULL)
+		if (*h != NULL)
 			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
