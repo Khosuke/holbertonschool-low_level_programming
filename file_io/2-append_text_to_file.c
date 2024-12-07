2855bf30ea699f0dd54cf33ebfae06ac3ff01a3b
Khosuke <9814@holbertonstudents.com>
2024-12-07 13:34:40 +0100

    removed unnecessary whitespace

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -4,15 +4,15 @@
  * append_text_to_file - appends text at the end of a file
  * @filename: the name of the file
  * @text_content:  string to add at the end of the file
- * 
+ *
  * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t w;
-	int fd,lentxt = 0;
+	int fd, lentxt = 0;
 
-	if (filename == NULL) 
+	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
